e74cec140e6d63562d40791710ed5f9d50a9fbf8
Arka <arkajyotiadhikary15@gmail.com>
2020-11-14 21:29:00 +0530

    Added random text selector and display

--- Min N Jam Game Jam/Assets/GameManager/RandomWordPicker.cs
+++ Min N Jam Game Jam/Assets/GameManager/RandomWordPicker.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomWordPicker : MonoBehaviour
 {
-    public 
+    public string[] words;
+    
+    public Text choosedText;
+
+
+    enum gameState
+    {
+        choosingWord,
+        writingWord
+    }
+
+    string choosedWord;
+    
+    gameState currentState;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +29,17 @@ public class RandomWordPicker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentState == gameState.choosingWord)
+        {
+            StartCoroutine(ChooseRandomWord());
+        }
+    }
+
+    IEnumerator ChooseRandomWord()
+    {
+        choosedWord = words[Random.Range(0,words.Length)];
+        choosedText.text = choosedWord;
+        currentState = gameState.writingWord;
+        yield return new WaitForSeconds(1);
     }
 }
