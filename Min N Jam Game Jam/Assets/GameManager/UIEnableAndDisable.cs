397a35649937f183c236f84cbfa29b0d3f4b0182
Arka <arkajyotiadhikary15@gmail.com>
2020-11-15 09:01:52 +0530

    inputfield Animation

--- Min N Jam Game Jam/Assets/GameManager/UIEnableAndDisable.cs
+++ Min N Jam Game Jam/Assets/GameManager/UIEnableAndDisable.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIEnableAndDisable : MonoBehaviour
 {
+    
+    public bool enableUI;
+
+    public GameObject inputField;
+    public GameObject wordDisplay;
+
+    InputField input;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        input = inputField.GetComponent<InputField>();
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(enableUI)
+        {
+            input.ActivateInputField();
+            inputField.SetActive(true);
+            wordDisplay.SetActive(true);
+        }
+        else{
+            StartCoroutine(DisableUI());
+        }
+    }
+    IEnumerator DisableUI()
+    {
+        inputField.GetComponent<Animator>().SetBool("Disable",true);
+        wordDisplay.GetComponent<Animator>().SetBool("Disable",true);
+        yield return new WaitForSeconds(1);
+        input.text = "";
+        inputField.SetActive(false);
+        wordDisplay.SetActive(false);
     }
 }
